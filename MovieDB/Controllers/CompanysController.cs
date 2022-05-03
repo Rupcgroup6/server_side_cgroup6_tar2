4406f9de03611e15aff76c3e6f284989ac6a5c42
Sahar Barzilay <sahar093@gmail.com>
2022-05-03 16:11:28 +0300

    Sahar
    Finally...

---
+++ MovieDB/Controllers/CompanysController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using MovieDB.Models;
+
+namespace MovieDB.Controllers
+{
+    public class CompanysController : ApiController
+    {
+        public IEnumerable<Company> Get()
+        {
+            Company company = new Company();
+            return company.get();
+        }
+
+
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public int Post([FromBody] Company company)
+        {
+            
+            return company.InsertCompany();
+             
+
+        }
+
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
